b160fce33885b287666041ec1a3f359f41d0dea9
jvirissimo <jvirissimo@gmail.com>
2020-06-03 11:47:38 -0500

    3 files added

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  host:{
+    role: 'banner'
+  }
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() {
+    console.log('Header constructor called');
+  }
+
+  ngOnInit() {
+  }
+}
